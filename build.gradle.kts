9166e89d063aba5abebd27cf510d98462b9964c1
Melaine Gérard <pro@melaine-gerard.fr>
2026-04-15 22:01:16 +0200

    Update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "org.camelia.studio.kiss.shot.acerola"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 application {
     mainClass = "org.camelia.studio.kiss.shot.acerola.KissShotAcerola"
 }
